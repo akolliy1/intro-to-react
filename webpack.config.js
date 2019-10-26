80a342dc4a988a6c6f811e4f73df48a905775960
akolade <adesanmiakoladedotun@gmail.com>
2019-10-26 21:21:14 +0100

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'js/bundle.js',
+    publicPath: '/',
+  },
   module: {
     rules: [
       {
